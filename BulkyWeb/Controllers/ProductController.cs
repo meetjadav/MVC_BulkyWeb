74f5da27443471cb9b2f9761dbb1f1ec03ad1904
meetjadav <meetj7780@gmail.com>
2024-09-15 20:32:28 +0530

    some required changes

--- BulkyWeb/Controllers/ProductController.cs
+++ BulkyWeb/Controllers/ProductController.cs
@@ -1,80 +1,150 @@
 ﻿using BulkyWeb.Data;
 using BulkyWeb.Models;
+using BulkyWeb.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace BulkyWeb.Controllers
 {
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _db;
+
         public ProductController(ApplicationDbContext db)
         {
             _db = db;
         }
+
+        // Index: Display list of products with categories
         public IActionResult Index()
         {
-            List<Product> Product = _db.Products.ToList();
-            return View(Product);
+            var viewModel = new ProductCategoryViewModel
+            {
+                Products = _db.Products.ToList(),
+                Categories = _db.Categories.ToList()
+            };
+            return View(viewModel);
         }
+
+        // Create: Display form to create a new product
         public IActionResult Create()
         {
-            return View();
+            var viewModel = new ProductCategoryViewModel
+            {
+                Categories = _db.Categories.ToList(),
+                Product = new Product()
+            };
+            return View(viewModel);
         }
+
+
         [HttpPost]
-        public IActionResult Create(Product obj)
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ProductCategoryViewModel viewModel)
         {
             if (ModelState.IsValid)
             {
-                _db.Products.Add(obj);
+                _db.Products.Add(viewModel.Product);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View();
 
+            viewModel.Categories = _db.Categories.ToList();
+            return View(viewModel);
         }
+
+        // Edit: Display form to edit a product
         public IActionResult Edit(int? id)
         {
-            if (id == 0 || id == null)
+            if (id == null || id == 0)
             {
                 return NotFound();
             }
-            else
+
+            var product = _db.Products.Find(id);
+            if (product == null)
             {
-                Product? item = _db.Products.Find(id);
-                return View(item);
+                return NotFound();
             }
 
+            var viewModel = new ProductCategoryViewModel
+            {
+                Product = product,
+                Categories = _db.Categories.ToList()
+            };
+            return View(viewModel);
         }
-        [HttpPost]
-        public IActionResult Edit(Product obj)
+
+        [HttpPost,ActionName("Edit")]
+        public IActionResult Edit(ProductCategoryViewModel viewModel)
         {
+            if (viewModel.Product.Id == 0)
+            {
+                return NotFound();
+            }
+
             if (ModelState.IsValid)
             {
-                _db.Products.Update(obj);
+                _db.Products.Update(viewModel.Product);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View();
+
+            viewModel.Categories = _db.Categories.ToList();
+            return View(viewModel);
         }
+
+
+
         public IActionResult Delete(int? id)
         {
-            if (id == 0 || id == null)
+            if (id == null || id == 0)
             {
                 return NotFound();
             }
-            else
+
+            var product = _db.Products.Find(id);
+            if (product == null)
             {
-                Product? item = _db.Products.Find(id);
-                return View(item);
+                return NotFound();
             }
 
+            var viewModel = new ProductCategoryViewModel
+            {
+                Product = product,
+                Categories = _db.Categories.ToList()
+            };
+            return View(viewModel);
         }
-        [HttpPost]
-        public IActionResult Delete(Product obj)
+
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
         {
-            _db.Products.Remove(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Products.Remove(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            var viewModel = new ProductCategoryViewModel
+            {
+                Product = product,
+                Categories = _db.Categories.ToList()
+            };
+            return View(viewModel);
         }
     }
 }
---
+++ BulkyWeb/ViewModels/ProductCategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BulkyWeb.Models;
+
+namespace BulkyWeb.ViewModels
+{
+    public class ProductCategoryViewModel
+    {
+        public List<Product>? Products { get; set; }
+        public List<Category>? Categories { get; set; }
+
+        public Product? Product { get; set; }
+    }
+}
